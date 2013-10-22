966c0553e92124a9f8e307aa25ddc4b81ff6c4a0
Andersen Fan <as181920@hotmail.com>
2013-10-22 16:14:15 +0800

    调试通和doorkeeper服务的对接

--- lib/omniauth/strategies/notes.rb
+++ lib/omniauth/strategies/notes.rb
@@ -1,27 +1,26 @@
-require 'omniauth'
+require 'omniauth/strategies/oauth2'
 
 module OmniAuth
   module Strategies
     class Notes < OmniAuth::Strategies::OAuth2
-
       option :name, :notes
-      option :fields, [:email, :id]
-      option :uid_field, :id
 
       option :client_options, {
         :site => "http://notes18.com",
         :authorize_url => "/oauth/authorize"
       }
 
-      uid do
-        request.params[options.uid_field.to_s]
-      end
+      uid { raw_info["id"] }
 
       info do
-        options.fields.inject({}) do |hash, field|
-          hash[field] = request.params[field]
-          hash
-        end
+        {
+          email: raw_info["email"]
+          # and anything else you want to return to your API consumers
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/api/me.json').parsed
       end
     end
   end
